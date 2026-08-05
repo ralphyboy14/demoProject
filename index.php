a00b93d4582491e1cf697d989a16a0f828b98b49
ralphyboy22 <lightandheavyrepairshop@gmail.com>
2026-08-05 09:18:24 +0800

    adding two numbers

--- index.php
+++ index.php
@@ -1,18 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <p1> Hello world</p1>
-    <p1> Hello world</p1>
-    <h1>   Hello world </h1>
-    <p1> Hello world</p1>
-    <p> add conflict from cloned</p>
-    <p> add conflict from cloned2</p>
-    <p> add conflict from original</p>
-    <p> add conflict from original2</p>
-</body>
-</html>
+<?php
+
+$num1 = readline("Enter the first number: ");
+$num2 = readline("Enter the second number: ");
+
+$sum = $num1 + $num2;
+echo "The sum of $num1 and $num2 is: $sum\n";
+
+
+?>

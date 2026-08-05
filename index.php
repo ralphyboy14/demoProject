e8aceab70fa3016d93333429aac07fee0bc97b43
ralphyboy22 <lightandheavyrepairshop@gmail.com>
2026-08-05 09:19:34 +0800

    difference of two numbers

--- index.php
+++ index.php
@@ -1,17 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <p1> Hello world</p1>
-    <p1> Hello world</p1>
-    <h1>   Hello world </h1>
-    <p1> Hello world</p1>
-    <p> add conflict from cloned</p>
-    <p> add conflict from cloned2</p>
-    <p> add conflict from original</p>
-</body>
-</html>
+<?php
+
+$num1 = readline("Enter the first number: ");
+$num2 = readline("Enter the second number: ");
+
+$d = $num1 - $num2;
+echo "The difference of $num1 and $num2 is: $d\n";
+
+
+?>
